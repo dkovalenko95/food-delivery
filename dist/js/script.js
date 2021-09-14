2e761038edef17025f64942a30ff087bfa25708e
Dmytro <dkovalenko2425@gmail.com>
2021-09-14 17:51:51 +0300

    modalWindow added

--- dist/js/script.js
+++ dist/js/script.js
@@ -201,7 +201,67 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  setClock('.timer', deadline);
+  setClock('.timer', deadline); // MODAL
+
+  const modalBtn = document.querySelectorAll('[data-modal]'),
+        modalClose = document.querySelector('[data-close]'),
+        modalWindow = document.querySelector('.modal');
+
+  function openModal() {
+    modalWindow.classList.add('show', 'fade');
+    modalWindow.classList.remove('hide'); // Либо вариант с toggle - но тогда назначить класс в верстке
+
+    document.body.style.overflow = 'hidden';
+    /* фиксация страницы, что бы не было прокрутки во время открытого модального окна */
+
+    clearInterval(modalTimerId);
+    window.removeEventListener('scroll', showModalScroll);
+    /* если мы во время просмотра сайта нажали кнопку вызова модального окна, то после этого события или после события setTimeout(), при прокрутке до конца сайта, модальное окно больше не вызывалось. Таким образом, модальное окно до конца прокрутки сайта, не вызовется, если было событие по таймеру или по клику. */
+  }
+
+  modalBtn.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+
+  function closeModal() {
+    modalWindow.classList.add('hide');
+    modalWindow.classList.remove('show'); // Либо вариант с toggle - но тогда назначить класс в верстке
+
+    document.body.style.overflow = '';
+    /* браузер ставит значение по дефолту */
+  }
+
+  modalClose.addEventListener('click', closeModal); // modalClose.addEventListener('click', () => {
+  //     modalWindow.style.display = 'none';
+  // });
+
+  modalWindow.addEventListener('click', event => {
+    if (event.target === modalWindow) {
+      closeModal();
+    }
+  });
+  document.addEventListener('keydown', e => {
+    if (e.code === 'Escape' && modalWindow.classList.contains('show')) {
+      closeModal();
+    }
+  });
+  const modalTimerId = setTimeout(openModal, 6000);
+
+  function showModalScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalScroll);
+    }
+  }
+  /* function showModalScroll () {
+      if (window.pageYOffset >= 3800) {
+          openModal();
+          window.removeEventListener('scroll', showModalScroll);
+      }
+  } */
+
+
+  window.addEventListener('scroll', showModalScroll);
 });
 
 /***/ })
