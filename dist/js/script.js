dcf86cf88be0845a1c0d7cad8e62b66536adb12f
Dmytro <dkovalenko2425@gmail.com>
2021-09-14 12:50:35 +0300

    getZero func added

--- dist/js/script.js
+++ dist/js/script.js
@@ -133,21 +133,42 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }); // TIMER
 
-  const deadline = '2021-09-16';
+  const deadline = '2021, 09, 15';
 
   function getTimeRemaining(endtime) {
     const t = Date.parse(endtime) - Date.parse(new Date()),
-          days = Math.floor(t / (1000 * 60 * 60 * 24)),
-          hours = Math.floor(t / (1000 * 60 * 60) % 24),
+
+    /* разница в миллисекундах */
+    days = Math.floor(t / (1000 * 60 * 60 * 24)),
+
+    /* Math.floor - округление числа */
+    hours = Math.floor(t / (1000 * 60 * 60) % 24),
           minutes = Math.floor(t / 1000 / 60 % 60),
           seconds = Math.floor(t / 1000 % 60);
     return {
       'total': t,
-      'days': days,
+
+      /* 'days': days,
       'hours': hours,
       'minutes': minutes,
-      'seconds': seconds
+      'seconds': seconds */
+
+      /* - старый синтаксис, до 2015 - ES5 */
+      days,
+      hours,
+      minutes,
+      seconds
+      /* - новый синтаксис, ES6 стандарт*/
+
     };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
   }
 
   function setClock(selector, endtime) {
@@ -157,16 +178,25 @@ window.addEventListener('DOMContentLoaded', () => {
           minutes = document.querySelector('#minutes'),
           seconds = document.querySelector('#seconds'),
           timeInterval = setInterval(updateClock, 1000);
+    updateClock();
+    /* запуск функции что бы предотвратить мигание на странице */
 
     function updateClock() {
       const t = getTimeRemaining(endtime);
-      days.innerHTML = t.days;
-      hours.innerHTML = t.hours;
-      minutes.innerHTML = t.minutes;
-      seconds.innerHTML = t.seconds;
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
 
       if (t.total <= 0) {
         clearInterval(timeInterval);
+      }
+
+      if (t.total < 0) {
+        days.innerHTML = '00';
+        hours.innerHTML = '00';
+        minutes.innerHTML = '00';
+        seconds.innerHTML = '00';
       }
     }
   }
